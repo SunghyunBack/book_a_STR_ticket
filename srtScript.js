a49857935bccd2b786bcf02c614fdb9f8792a450
SunghyunBack <128347334+SunghyunBack@users.noreply.github.com>
2025-04-15 16:21:46 +0900

    Update srtScript.js

--- srtScript.js
+++ srtScript.js
@@ -1,85 +1,57 @@
-// content script로 보며 이름 변경은 가능하다.
-// 이 파일을 이용해서 현재 페이지의 DOM에서 특정 Element의 정보를 얻거나,
-//  얻은 정보로 DOM을 조작할수 있게 해준다.
-//  createElement로 Element를 생성하여 원하는 UI를 추가할수도 있다.
+let idOfTimeOut;
+let inProgress = false;
+const refreshInterval = 2200; //ms
 
-var idOfTimeOut;
-var inProgress = false;
-const refreshInterval = 2200; //ms 새로고침에 대한 시간을 가리킨다.
-
-// 버튼을 만들고 페이지의 상태마다 필요한 내용에 대한 함수를 정의했다.
 function makeStartButton() {
     var startButton = document.createElement("button");
-    // class 의 값들은 부트스트랩 값들이다.
     startButton.setAttribute("class", "btn_large wx200 btn_burgundy_dark2 val_m corner");
     startButton.setAttribute("type", "button");
     startButton.setAttribute("id", "mStartButton");
 
-    if (globalFlag) {
+    if (inProgress) {
         startButton.innerText = "중지";
     } else {
         startButton.innerText = "시작";
     }
 
-    // 시작, 중지 그리고 1개라도 선택 되어있지 않다면 작동하지 않게 하기 위한 이벤트핸들러를 정의했다.
     startButton.addEventListener("click", function (event) {
-        if (globalFlag) {
+        if (inProgress) {
             updateStatus(false);
-            startButton.innerText="시작";
+            startButton.innerText = "시작";
+        } else if (atLeastOneCheck()) {
+            updateStatus(true);
+            startButton.innerText = "중지";
         } else {
-            if (alLeastOneCheck()) {
-                updateStatus(true);
-                startBUtton.innerText="중지";
-            } else {
-                alert("체크된 항목이 없습니다.")
-            }
+            alert("체크된 항목이 없습니다.");
         }
     });
+
     document.getElementsByClassName("sub_con_area")[0].appendChild(startButton);
     return startButton;
 }
 
-
-
-
-// 새로고침을 위한 함수 구현이다.
-// timeoutPeriod은 타임아웃주긴 인자이다.
-// 여기서 알아야 하는게 함수를 정의할때 인자값에 대해서 조금더 생각해바야한다.!!!
 function refreshPageAfter(timeoutPeriod) {
-    // location.reload()메소드는 페이지 새로고침을 할때 사용한다.
-    // 그중 true로 인해 캐시가 있더라도 서버의 최신 콘텐츠를 얻을수 있다.
-    //  단점으로 콘텐츠를 날려 데이터를 잃거나 깜빡거려 경험에 영향을 줄수 있다.
-    // setTimeOut()는 어떤 코드를 바로 실행하지 않고 일정 시간 기다린 후 실행하고 싶을때 사용하는 코드이다.
     let reloadButton = document.getElementById("search_top_tag").getElementsByTagName("input")[0];
-    idOfTimeOut = setTimeout(function () {reloadButton.click()}, timeoutPeriod);
+    idOfTimeOut = setTimeout(function () { reloadButton.click() }, timeoutPeriod);
 }
 
-
-
-//  updateStatus()에 대한 함수를 정의한다.
 function updateStatus(status) {
-    // 체크되어진 박스를 담을 list이다.
-    let firstClassList = [];
-    let economyClassList = [];
-
+    let firstClassList = []
+    let economyClassList = []
 
     if (status) {
-    // 나중에 만들어질 체크 박스를 의미한다.
-    var checkboxesForFirstClass = document.getElementsByClassName("mCheckboxForFirstClass");
-    var checkboxesForEconomyClass = document.getElementsByClassName("mCheckboxForEconomyClass");
-        for (var i = 0; i < checkboxesForFirstClass.length; ++i) {
+        let checkboxesForFirstClass = document.getElementsByClassName("mCheckboxForFirstClass");
+        let checkboxesForEconomyClass = document.getElementsByClassName("mCheckboxForEconomyClass");
+
+        for (let i = 0; i < checkboxesForFirstClass.length; ++i) {
             if (checkboxesForFirstClass[i].checked) {
-                // 웹페이지 상 parent는 1행을 뜻한다.
-                // 웹페이지 상 trainNumber은 열차번호를 뜻한다. 그래서
-                //  웹페이지의 구조를 보고 checkboxesForFirstClass의 순서는 상관없지만 두번 거슬러 올라가면 tr을 가리킬수 있다.
-                //  가리킨 tr에서 2번째 자식은 언제나 trainNumber을 뜻한다.
-                const parent = checkboxesForFirstClass[i].parentElement.parentElement; // tr td input 이라서 두번 거슬러 올라간다.
+                const parent = checkboxesForFirstClass[i].parentElement.parentElement; //tr td input 이라서 두번 거슬러 올라감
                 const trainNumber = parent.children[2].innerText;
                 firstClassList.push(trainNumber);
             }
         }
 
-        for (var i = 0; i < checkboxesForEconomyClass.length; ++i) {
+        for (let i = 0; i < checkboxesForEconomyClass.length; ++i) {
             if (checkboxesForEconomyClass[i].checked) {
                 const parent = checkboxesForEconomyClass[i].parentElement.parentElement;
                 const trainNumber = parent.children[2].innerText;
@@ -88,12 +60,6 @@ function updateStatus(status) {
         }
     }
 
-    // chrome.storage API이다. 외우기 보다는 어떻게 활용하고 사용할수 잇는지를 공부해야한다.
-    // 그리고 이 API를 사용하기 위해서는 매니페스트에서 권환을 선언해야한다.!!!!!
-
-    // chrome.storage.syne.set을 사용하여 데이터를 저장한다.
-    // mData라는 키에 객체로 저장한다.
-    //  저장 완료 후 콜백 함수를 호출한다.
     chrome.storage.sync.set({
         mData: {
             flag: status,
@@ -101,38 +67,37 @@ function updateStatus(status) {
             economyClassList: economyClassList
         }
     }, function () {
-        inProgress =status;
+        inProgress = status;
         if (status) {
             location.reload(true);
         } else {
             clearTimeout(idOfTimeOut);
         }
-        console.log("status setting completed : " + status + ", firstClassList : " + firstClassList + ", economyClassList : " + economyClassList);
+        console.log("status setting completed: " + status + ", firstClassList:" + firstClassList + ", economyClassList:" + economyClassList);
     });
 }
 
-
-// 대상들을 지정 및 생성?
+//매크로 대상들도 필요할 듯
 function doJob(firstClassList, economyClassList) {
     if (inProgress) {
-        let sleepTime = 3000+Math.random()*3000;
-        console.log("sleep time:"+ sleepTime);
-        // window.onload는 웹 브라우저 내의 모든 요소가 준비가 되어야 실행 할수 있도록 하는것이다.
-        // 웹 브라우저 내의 모든 요소가 준비된 이후 지정된 시간이 지나면 새로고침을 하겠다는 의미이다.
-        window.onload = refreshPageAfter(sleepTime);
+        let sleepTime = 3000 + Math.random() * 3000;
+        console.log("sleep time:" + sleepTime)
+        window.onload = refreshPageAfter(sleepTime); //random for [2,4] seconds
     }
 
     let startButton = makeStartButton();
 
     let tbodyList = document.getElementsByTagName("tbody");
-    if(tbodyList.length ==0 && inProgress){
-        startButton.InnerText = "시작";
-        updatesStatus(false);
+    if (tbodyList.length == 0 && inProgress) {
+        startButton.innerText = "시작";
+        updateStatus(false);
         return;
     }
+
     let searchedRows = tbodyList[0].children;
-    let targetButtons = [];
-    for(var i=0; i<searchedRows.length; ++i){
+    let targetButtons = []
+
+    for (var i = 0; i < searchedRows.length; ++i) {
         let row = searchedRows[i];
         let trainNumber = row.children[2].innerText;
         let tdForFirstClass = row.children[5];
@@ -140,66 +105,95 @@ function doJob(firstClassList, economyClassList) {
 
         let checkBtn = document.createElement("input");
         checkBtn.setAttribute("type", "checkbox");
-        checkBtn.setAttribute("class","mCheckboxForFirstClass");
+        checkBtn.setAttribute("class", "mCheckboxForFirstClass");
 
-        if(firstClassList.includes(trainNumber){
-            checkBun.checked =true;
+        if (firstClassList.includes(trainNumber)) {
+            checkBtn.checked = true;
             targetButtons.push(tdForFirstClass.getElementsByTagName("a")[0]);
         }
         tdForFirstClass.insertBefore(checkBtn, tdForFirstClass.firstChild);
 
         let checkBtn2 = document.createElement("input");
-        checkBtn2.setAttribute("type","checkbox");
-        checkBtn2.setAttribute("class","mCheckboxForEconomyClass");
+        checkBtn2.setAttribute("type", "checkbox");
+        checkBtn2.setAttribute("class", "mCheckboxForEconomyClass");
 
-        if(economyClassList.Includes(trainNumber){
-            checkBtn2.checked =true;
+        if (economyClassList.includes(trainNumber)) {
+            checkBtn2.checked = true;
             targetButtons.push(tdForEconomyClass.getElementsByTagName("a")[0]);
         }
         tdForEconomyClass.insertBefore(checkBtn2, tdForEconomyClass.firstChild);
 
         checkBtn.onclick = canNotModifyCheckBox;
         checkBtn2.onclick = canNotModifyCheckBox;
-        
     }
 
-    if(targetButtons.length <=0){
-        console.log("flag down");
+    if (targetButtons.length <= 0) {
+        console.log("flag down")
         updateStatus(false);
     }
 
-    for(let i=0; i<targetButtons.length; ++i){
-        let onClickAttr = targetButtons[i].getAttribute('onclick');
-        if(onClickAttr && onClickAttr.startsWith("reservationAfterMsg")){
-            targetButtons[i].setAttribute("onclick", onClickAttr.replace("MRT200164", "");
+    for (let i = 0; i < targetButtons.length; ++i) {
+        let onClickAttr = targetButtons[i].getAttribute('onclick')
+        if (onClickAttr && onClickAttr.startsWith("reservationAfterMsg")) { //reservationAfterMsg method: 예매하기, requestReservationInfoAnn method: 입석+좌석
+            targetButtons[i].setAttribute("onclick", onClickAttr.replace("MRT200164", "")); //잘 적용이 안되네
             targetButtons[i].click();
             updateStatus(false);
+            // chrome.runtime.sendMessage({type: 'playSuccessAudio'}, function(data) { });
+            // var audio = new Audio();
+            // audio.src = chrome.runtime.getURL('success.wav');
+            // audio.play();
+            // console.log("after sendMsg");
         }
     }
 }
 
+function canNotModifyCheckBox() {
+    if (inProgress) {
+        alert("매크로 동작동안 체크박스를 수정할 수 없습니다.");
+        return false;
+    }
+}
 
-// chrome.storage API이다. 외우기 보다는 어떻게 활용하고 사용할수 잇는지를 공부해야한다.
-// 그리고 이 API를 사용하기 위해서는 매니페스트에서 권환을 선언해야한다.!!!!!
-// chrome.storage.syne.get을 사용하여 저장된 데이터를 가지고 온다.
+function atLeastOneCheck() {
+    const checkboxesForFirstClass = document.getElementsByClassName("mCheckboxForFirstClass");
+    const checkboxesForEconomyClass = document.getElementsByClassName("mCheckboxForEconomyClass");
+
+    for (var i = 0; i < checkboxesForFirstClass.length; ++i) {
+        if (checkboxesForFirstClass[i].checked) {
+            return true;
+        }
+    }
+
+    for (var i = 0; i < checkboxesForEconomyClass.length; ++i) {
+        if (checkboxesForEconomyClass[i].checked) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 chrome.storage.sync.get('mData', function (result) {
     inProgress = false;
-    var firstClassList = [];
-    var economyClassList = [];
+    let firstClassList = [];
+    let economyClassList = [];
 
     if (result.mData) {
         inProgress = result.mData.flag;
 
-
         if (Array.isArray(result.mData.firstClassList)) {
             firstClassList = result.mData.firstClassList;
         }
+
         if (Array.isArray(result.mData.economyClassList)) {
             economyClassList = result.mData.economyClassList;
         }
     }
 
-    console.log("flag:" + inProgress + ", fList:" + firstClassList + ", eList:" + economyClassList);
+    console.log("inProgress:" + inProgress + ", fList:" + firstClassList + ", eList:" + economyClassList);
     doJob(firstClassList, economyClassList);
 });
+
+// 각 줄 가져오는거: $('#result-form fieldset div.tbl_wrap table tbody tr').length;
+// $('#result-form fieldset div.tbl_wrap table tbody tr td:nth-child(6)').length;
+// $('#result-form fieldset div.tbl_wrap table tbody tr td:nth-child(7)').length;
